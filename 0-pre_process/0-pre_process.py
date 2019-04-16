486c00defbd88fc69a8235e346139ce2b941a809
jaschen <cchenzyy@icloud.com>
2019-04-16 23:39:13 +0800

    完善预处理过程，导出处理之后的图片

--- 0-pre_process/0-pre_process.py
+++ 0-pre_process/0-pre_process.py
@@ -1,22 +1,53 @@
 import cv2
 import numpy as np
 import matplotlib.pylab as plt
+import os
+import random
+
 '''
 todo:读取一张图片，切分称为四部分，文件名字表示标签。然后存储到文件夹中。
 注意 文件命名格式。{num}_{str}.png size = 24 * 30 
 
 '''
+name = "0000.png"
+
 def cut_pic(img):
     num1 = img[0:46, 0:30]
     num2 = img[0:46, 31:60]
     num3 = img[0:46, 61:90]
     num4 = img[0:46, 91:120]
-    return list(num1, num2, num3, num4)
-def img2gray(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2GRAY)
-    img = cv2.GaussianBlur(img, (1, 1))
+    return [num1, num2, num3, num4]
+
+
+def img2gray(i_img):
+    i_img = cv2.GaussianBlur(i_img, (1, 1), 1)
     # todo:去除躁点，增加亮度，锐化。膨胀处理
-    img = cv2.cvtColor(img, -1)
-    return img
+    i_img = cv2.cvtColor(i_img, cv2.COLOR_BGR2GRAY)
+    i_img = cv2.adaptiveThreshold(i_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 17, 8)  # 二值化
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 7))  # 定义核 然后进行闭运算
+    cv2.morphologyEx(i_img, cv2.MORPH_CLOSE, kernel)
+    return i_img
+
+def save_img(img,lable):
+    cv2.imwrite(os.path.join(output_dir, lable + "_" + str(random.randint(0, 9999999)) + ".png"), img)
+
+
+
+root_dir = "/Users/jaschen/Desktop/data/images/"
+output_dir = "/Users/jaschen/Desktop/data/train/"
+list = os.listdir(root_dir)
+for i in range(0, len(list)):
+    path = os.path.join(root_dir, list[i])
+    name = path[::-1]
+    name = name[7:11]
+    name = name[::-1]
+    if os.path.isfile(path):
+        image = cv2.imread(path)  # 读取文件，然后调用切割
+        list_images = cut_pic(image)  # 首先切割图片，取得所有的列表。
+        for j in range(0, len(list_images)):
+            # todo:将单个的照片进行灰度处理+边缘检测+膨胀+腐蚀，然后保存
+            img = img2gray(list_images[j])
+            if name[j] != "_":
+                save_img(img, name[j])
 
 
